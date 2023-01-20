475bffcddc060f40c7bf690a49375351c382eb6f
Osinaike Gbemileke <osinaikegbemileke@gmail.com>
2023-01-20 15:32:09 +0100

    postgres db

--- IMS/settings.py
+++ IMS/settings.py
@@ -82,13 +82,22 @@ WSGI_APPLICATION = 'IMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'IMS', 
+        'USER': 'postgres',
+        'PASSWORD': 'osigbemes',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
